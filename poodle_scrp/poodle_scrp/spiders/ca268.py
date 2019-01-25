56382951b64ab3ca00007cfa956d8c1faf2161ca
teabolt <tomasbaltrunas0@gmail.com>
2019-01-25 00:35:41 +0000

    Flattened and normalised data returned by spider parsers

--- poodle_scrp/poodle_scrp/spiders/ca268.py
+++ poodle_scrp/poodle_scrp/spiders/ca268.py
@@ -26,53 +26,85 @@ class Ca268Spider(scrapy.Spider):
 
     def parse(self, response):
         sections = response.css('.topics > li')
-        for section in sections:
-            title = section.css('.section-title a ::text').extract_first()
-            if title is not None:
-                title = title.strip()
-            summary = ' '.join(section.css('.summarytext p ::text').extract()).replace('\r\n', '').strip()
-            yield {
-                'section_title': title,
-                'section_summary': summary,
-            }
+        # for section in sections:
+        #     title = section.css('.section-title a ::text').extract_first()
+        #     if title is not None:
+        #         title = title.strip()
+        #     summary = ' '.join(section.css('.summarytext p ::text').extract()).replace('\r\n', '').strip()
+        #     yield {
+        #         'section_title': title,
+        #         'section_summary': summary,
+        #     }
 
-        for section_link in sections.css('.section-title a')[10:11]:
+        for section_link in sections.css('.section-title a')[-1:-4:-2]:
             section_request = response.follow(
                 section_link, 
                 callback=self.parse_section,
             )
-            section_request.meta['original_section_url'] = section_link.xpath('@href').extract_first()
+            section_request.meta['url_to_section'] = section_link.xpath('@href').extract_first()
             yield section_request
 
     def parse_section(self, response):
-        # to identify the main content in the response, need to know the section id
-        section_url = response.meta['original_section_url']
-        url_qs = urllib.parse.parse_qs(urllib.parse.urlparse(section_url).query)
-        section_content = response.css('#section-{} .section'.format(url_qs['section'][0]))
-        # section_content = response.css('#section-9 .section')
-        vpls = section_content.css('.vpl')
-        for vpl in vpls[:3]:
-            yield {'vpl_title': vpl.css('::text').extract_first()}
+        # need to know the section id in order to identify the main content of the page
+        section_url = response.meta['url_to_section']
+        # section_url = 'https://poodle.computing.dcu.ie/moodle/course/view.php?id=4&section=4'
+        querystring = urllib.parse.parse_qs(urllib.parse.urlparse(section_url).query)
+        section_identifier = '#section-{}'.format(querystring['section'][0])
+        section_title = response.css('#region-main .navigationtitle .sectionname ::text')
+        section_summary = response.css('{} .summary ::text'.format(section_identifier))
+        yield {
+            'type':'section',
+            'section_title': section_title.extract_first(),
+            'section_summary': ' '.join(section_summary.extract()).replace('\r\n', '').strip(),
+        }
 
-        for vpl_a in vpls.css('a')[:3]:
+        # for vpl in vpls:
+        #     yield {'vpl_title': vpl.css('::text').extract_first()}
+
+        for vpl_a in response.css('{} .vpl a'.format(section_identifier)):
             yield response.follow(
                 vpl_a,
                 callback=self.parse_vpl)
 
     def parse_vpl(self, response):
-        main = response.css('.box > .box')
-        text = main.css('p, pre').css('::text')
-        description = text.extract()
-        yield {
-            'vpl_description': '\n'.join(description)
+        """Follow links to the Virtual Programming Labs"""
+        vpl_title = response.css('[role="main"] > h2::text')
+        vpl_body = response.css('.box > .box')
+        vpl_description = vpl_body.css('p, pre').css('::text')
+        vpl_item = {
+            'type': 'vpl',
+            'vpl_title': vpl_title.extract_first(),
+            'vpl_description': '\n'.join(vpl_description.extract())
         }
 
-        submissionview = response.css('[role="main"] .nav [title="Submission view"]')
-        if submissionview:  # if non-empty list
-            yield response.follow(submissionview[0], callback=self.parse_submissionview)
+        submissionview_link = response.css('[role="main"] .nav [title="Submission view"]')
+        if submissionview_link:  # if non-empty list
+            submission_request = response.follow(submissionview_link[0], callback=self.parse_submissionview)
+            submission_request.meta['vpl_item'] = vpl_item
+            yield submission_request
 
     def parse_submissionview(self, response):
         # parsing must happen before JavaScript is ran on the DOM (on the raw HTML sourcecode) (afterards, Ace Editor styles and distributes the code over many elements)
-        code = response.css('[role="main"] #codefileid1 ::text') 
-        yield {'vpl_code': code.extract_first()}
-        # code to get upload / test info ...
+        vpl_item = response.meta['vpl_item']
+        # vpl_item = {
+        # 'type':'vpl',
+        # 'vpl_title':'How many items',
+        # 'vpl_description':'abababa test',
+        # }
+        code = response.css('[role="main"] #codefileid1 ::text')
+        vpl_item['vpl_code'] = code.extract_first()
+
+        assessment = response.css('.box')
+        tests = assessment.re_first('[^|]*run[^|]*')
+        if tests:
+            tests = tests.strip()
+        vpl_item['vpl_tests'] = tests
+
+        grade = assessment.xpath('//b[text()="grade"]/following-sibling::text()[1]').re_first('[0-9]+')
+        try:
+            grade = int(grade)
+        except ValueError:
+            pass
+        vpl_item['vpl_grade'] = grade
+
+        yield vpl_item
